1b5566cdc0e6b2b43d9147af3383bd911649baec
Kevin Towner <>
2020-12-31 16:46:22 -0500

    Save

--- assets/js/analytics/analytics.js
+++
@@ -1,9 +0,0 @@
-
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-0P7SDXF3EZ"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-0P7SDXF3EZ');
-</script>
